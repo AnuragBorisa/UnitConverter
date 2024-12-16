92e18f6b1770eb54f1da9b58cc1f263cdc068a8f
Anurag Borisa <anuragborisa@gmail.com>
2024-12-16 21:32:42 +0530

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Anurag on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
